eea0f5db7f04ed6cb0480bbf704f593621aea796
agroyz <agroyz@vectra.ai>
2023-02-12 17:16:33 -0500

    update to output

--- scenarios/cloud_breach_s3/terraform/outputs.tf
+++ scenarios/cloud_breach_s3/terraform/outputs.tf
@@ -6,3 +6,6 @@ output "cloudgoat_output_aws_account_id" {
 output "cloudgoat_output_target_ec2_server_ip" {
   value = "${aws_instance.ec2-vulnerable-proxy-server.public_ip}"
 }
+output "cloudgoat_output_s3_bucket" {
+  value = "${aws_s3_bucket.cg-cardholder-data-bucket.id}"
+}
